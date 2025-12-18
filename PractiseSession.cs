0aa4c205ab6e86af90650086b38288cb9dde645d
Malte95 <maltescherenberg@gmx.de>
2025-12-18 15:14:09 +0100

    Initial project setup (Phase + PracticeSession)

---
+++ PractiseSession.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class PractiseSession
+{
+    private readonly List<Phase> _phases = new();
+
+    public IReadOnlyList<Phase> Phases => _phases;
+
+    public void AddPhase(Phase phase)
+    {
+        _phases.Add(Phase);
+    }
+}
---
+++ PractiseTimer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PractiseTimer
+{
+    class Program
+    {
+        public class Phase
+        {
+            public string Name { get; set; } = "";
+            public int DurationMinutes { get; set; }
+        }
+    }
+}
